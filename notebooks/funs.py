878b3d0309549f87ba86fb54de8e2fa288bda098
tomaszzywicki <tomasz.zywicki03@gmail.com>
2024-04-22 21:28:56 +0200

    add best models scores

--- notebooks/funs.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-
-def evaluate_accuracy(model, X, y, X_test, y_test):
-    model.fit(X, y)
-    y_pred = model.predict(X_test)
-    print("Accuracy: ", accuracy_score(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
-    return 
